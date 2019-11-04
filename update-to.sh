3a6dee0230c93ec626492a24e19e80412f3b5623
michaelknurr <33127160+michaelknurr@users.noreply.github.com>
2019-11-04 15:39:13 +0100

    Update update-to.sh

--- update-to.sh
+++ update-to.sh
@@ -8,20 +8,22 @@ fi
 cd $(dirname $0)
 VERSION=$1
 
-if [ "$VERSION" == "latest" ] ; then        
+if [ "$VERSION" == "latest" ] ; then
         # update to latest version
         mkdir -p upgrade
         cd upgrade
         rm -f jenkins-cli.jar
         wget http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
         CURRENT_VERSION=$( java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ version);
+        echo "Current version: [$CURRENT_VERSION]"
 
-        # get the latest stable jenkins release 
+        # get the latest stable jenkins release
         rm -f jenkins.war
         wget  wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
         LATEST_VERSION=$( java -jar jenkins.war --version );
-        
-        if [ $CURRENT_VERSION == $LATEST_VERSION ]; then
+        echo "Latest version: [$LATEST_VERSION]"
+
+        if [ "$CURRENT_VERSION" == "$LATEST_VERSION" ]; then
                 echo "No updates available"
                 exit 0
         fi
@@ -29,7 +31,7 @@ if [ "$VERSION" == "latest" ] ; then
         VERSION=$LATEST_VERSION
 
 else
-        # update to specific version        
+        # update to specific version
         if [ -d $VERSION ] ; then
                 echo "Version already downloaded"
                 exit 1
@@ -37,9 +39,10 @@ else
         mkdir -p upgrade
         cd upgrade
         wget http://updates.jenkins-ci.org/download/war/$VERSION/jenkins.war
-        
+
 fi
 
+echo "Installing version: [$VERSION]"
 cd ..
 mkdir $VERSION
 mv upgrade/jenkins.war $VERSION
